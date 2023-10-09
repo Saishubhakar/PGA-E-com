ed06e2a50b7004bc9a9bc4974589e1fe32332a54
unknown <saishubhakar97@gmail.com>
2023-10-09 22:30:29 +0530

    Project completed

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,103 +1,76 @@
-import './Home.css';
-import './Images.css'
-import './About.css'
-import './SignUp.css'
-import './Categories.css'
-import './Cart.css'
-import './Settings.css'
+import "./Home.css";
+import "./Images.css";
+import "./About.css";
+import "./SignUp.css";
+import "./Categories.css";
+import "./Cart.css";
+import "./Settings.css";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Navbar from './Additions/Navbar';
-import SignUp from './Additions/SignUp';
-import About from './Additions/About';
-import Onepiece from './Additions/Onepiece';
-import Footer from './Additions/Footer';
-import Categories from './Additions/Categories';
-import Cart from './Additions/Cart';
-import Login from './Additions/Login';
-import Settings from './Additions/Settings';
-import DeleteUser from './Additions/DeleteUser';
-import ProductCreate from './Additions/Productcreate';
-import CardTemplate from './Additions/CardTemplate';
+import Navbar from "./Additions/Navbar";
+import SignUp from "./Additions/SignUp";
+import About from "./Additions/About";
+import PGA from "./Additions/PGA";
+import Footer from "./Additions/Footer";
+import Categories from "./Additions/Categories";
+import Cart from "./Additions/Cart";
+import Login from "./Additions/Login";
+import Settings from "./Additions/Settings";
+import DeleteUser from "./Additions/DeleteUser";
+import ProductCreate from "./Additions/Productcreate";
+import CardTemplate from "./Additions/CardTemplate";
+import Protected from "./Additions/Protected";
 
 function App() {
-
-  function searching(){
-    const searcher = document.getElementById("input");
-    searcher.addEventListener("input", (e)=>{
-      const value = e.target.value
-      // console.log(value)
-      let inputValue = searcher.value
-      console.log(inputValue)
-      let cardBody = document.getElementsByClassName("card-body").innerText
-      // Array.from(cardBody).forEach((element)=>{
-      //   let cardTitle = element.getElementsByTagName("h4");
-      //   console.log(cardTitle)
-      // })
-      
-      if (inputValue.includes(cardBody)){
-        console.log("MatchFound")
-      }
-      else{
-        console.log("something went wrong")
-      }
-    });
-  }
-
+  const loggedIn = localStorage.getItem("LoggedIn");
+  console.log(loggedIn);
 
   return (
     <Router>
       <div className="App">
-        <Navbar/>
+        <Navbar />
         <Switch>
-
           <Route path="/about">
             <About />
           </Route>
 
-          <Route path="/OnePiece">
-            <Onepiece searchFunc = {searching}/>
-          </Route>
+          <Route path="/PGA" exact component = {loggedIn ? PGA : Login}/>
 
           <Route path="/sign-up">
             <SignUp />
           </Route>
 
-          <Route path = "/login">
+          <Route path="/login">
             <Login/>
           </Route>
 
-          <Route path = "/User-delete">
-            <DeleteUser/>
+
+          <Route path="/User-delete">
+            <DeleteUser />
           </Route>
 
           <Route path="/cart">
-            <Cart/>
+            <Cart />
           </Route>
 
-          <Route path = "/settings">
-            <Settings/>
+          <Route path="/settings">
+            <Settings />
           </Route>
 
-          <Route path = "/productCreate">
-            <ProductCreate/>
+          <Route path = "/sellProducts">
+            <ProductCreate />
           </Route>
 
-          <Route path = "/cardTemplate">
-            <CardTemplate/>
+          <Route path="/cardTemplate">
+            <CardTemplate />
           </Route>
 
           <Route path="/">
             <Categories />
           </Route>
-          
         </Switch>
-        <Footer/>
+        <Footer />
       </div>
     </Router>
   );
