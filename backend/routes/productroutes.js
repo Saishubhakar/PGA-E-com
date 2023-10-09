a0c8fcc9d6410164ea89aaa47e1fb342d13ab1e7
unknown <saishubhakar97@gmail.com>
2023-10-09 21:39:01 +0530

    Project completed

--- backend/routes/productroutes.js
+++ backend/routes/productroutes.js
@@ -6,10 +6,11 @@ const {
     createProducts,
     getAllProducts,
     getAllProductsStatic,
+    searchAPI,
 } = require("../controllers/products")
 
 router.route("/getproducts").get(getProducts)
 router.route("/createproducts").post(createProducts)
-router.route("/getAllProducts").get(getAllProducts)
+// router.route("/getAllProducts").get(getAllProducts)
 
 module.exports = router
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -2,10 +2,12 @@ const express = require("express")
 const router = express.Router()
 const {registerUser, 
     loginUser,
-    deleteUser,} = require("../controllers/pages")
+    deleteUser,
+    protectedData} = require("../controllers/pages")
 
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
 router.route("/delete").delete(deleteUser)
+router.route("/protected-data").get(protectedData)
 
 module.exports = router
